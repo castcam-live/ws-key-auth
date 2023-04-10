1f45ccf7c392f993e936b92fc0f3f48e8412bb10
Sal Rahman <salehen.rahman@gmail.com>
2023-04-09 23:29:59 -0700

    Some initial work

---
+++ go/example/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/gorilla/websocket"
+
+	wskeyauth "github.com/clubcabana/ws-key-auth/go"
+)
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		upgrader := websocket.Upgrader{}
+		conn, err := upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		defer conn.Close()
+
+		authenticated, err := wskeyauth.Handshake(conn)
+
+		if !authenticated || err != nil {
+			log.Println("Failed to authenticate")
+			return
+		}
+
+		log.Println("Authenticated")
+
+		for {
+			_, message, err := conn.ReadMessage()
+			if err != nil {
+				log.Println("read:", err)
+				break
+			}
+			log.Printf("recv: %s", message)
+		}
+	})
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
---
+++ go/lib.go
@@ -0,0 +1,177 @@
+package wskeyauth
+
+import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"encoding/base64"
+	"encoding/json"
+	"errors"
+	"math/big"
+	"strings"
+
+	"github.com/gorilla/websocket"
+)
+
+type TypeData struct {
+	Type string          `json:"type"`
+	Data json.RawMessage `json:"data"`
+}
+
+// <- CLIENT_ID
+// -> CHALLENGE
+// <- CHALLENGE_RESPONSE
+// And then either:
+//   -> SIGNATURE_MATCHES
+//   or
+//   -> SIGNATURE_MISMATCH
+
+// A client ID will be of the format
+//
+// WebCrypto-raw.EC.<named curve>$<base64 encoded public key>
+
+func ErrInvalidClientID() error {
+	return errors.New("invalid client ID")
+}
+
+func ErrFailedToReadRandomNumbers() error {
+	return errors.New("failed to read random numbers")
+}
+
+func parseClientID(clientID string) (*ecdsa.PublicKey, error) {
+	s := strings.Split("$", clientID)
+	if len(s) != 2 {
+		return nil, ErrInvalidClientID()
+	}
+
+	if s[0] != "WebCrypto-raw.EC.P-256" {
+		return nil, ErrInvalidClientID()
+	}
+
+	buff, err := base64.StdEncoding.DecodeString(s[1])
+	if err != nil {
+		return nil, err
+	}
+
+	if len(buff) != 65 {
+		return nil, ErrInvalidClientID()
+	}
+
+	if buff[0] != 4 {
+		return nil, ErrInvalidClientID()
+	}
+
+	x := &big.Int{}
+	y := &big.Int{}
+
+	x.SetBytes(buff[1:33])
+	y.SetBytes(buff[33:])
+
+	return &ecdsa.PublicKey{
+		X:     x,
+		Y:     y,
+		Curve: elliptic.P256(),
+	}, nil
+}
+
+const challengeByteLength = 128
+
+// It will be safe to assume that any error coming from this function is a client
+func getChallengePayload() (b []byte, err error) {
+	b = make([]byte, challengeByteLength)
+	n, err := rand.Read(b)
+	if err != nil {
+		return []byte{}, err
+	}
+	if n < challengeByteLength {
+		return []byte{}, ErrFailedToReadRandomNumbers()
+	}
+	return b, nil
+}
+
+func Handshake(conn *websocket.Conn) (bool, error) {
+	var td TypeData
+	err := conn.ReadJSON(&td)
+	if err != nil {
+		return false, err
+	}
+
+	if td.Type != "CLIENT" {
+		return false, nil
+	}
+
+	var clientID string
+	err = json.Unmarshal(td.Data, &clientID)
+	if err != nil {
+		return false, err
+	}
+
+	pubKey, err := parseClientID(clientID)
+
+	if err != nil {
+		return false, err
+	}
+
+	if pubKey == nil {
+		return false, nil
+	}
+
+	payload, err := getChallengePayload()
+	if err != nil {
+		return false, err
+	}
+
+	challenge := base64.StdEncoding.EncodeToString(payload)
+
+	if err != nil {
+		return false, err
+	}
+
+	conn.WriteJSON(map[string]string{
+		"type": "CHALLENGE",
+		"data": challenge,
+	})
+
+	err = conn.ReadJSON(&td)
+	if err != nil {
+		return false, err
+	}
+
+	if td.Type != "CHALLENGE_RESPONSE" {
+		return false, nil
+	}
+
+	var challengeResponse string
+	err = json.Unmarshal(td.Data, &challengeResponse)
+	if err != nil {
+		return false, err
+	}
+
+	decodedChallengeResponse, err := base64.StdEncoding.DecodeString(challengeResponse)
+	if err != nil {
+		return false, err
+	}
+
+	if len(decodedChallengeResponse) != 64 {
+		return false, nil
+	}
+
+	r := &big.Int{}
+	s := &big.Int{}
+
+	r.SetBytes(decodedChallengeResponse[:32])
+	s.SetBytes(decodedChallengeResponse[32:])
+
+	if !ecdsa.Verify(pubKey, payload, r, s) {
+		conn.WriteJSON(map[string]string{
+			"type": "SIGNATURE_MISMATCH",
+		})
+		return false, nil
+	}
+
+	conn.WriteJSON(map[string]string{
+		"type": "SIGNATURE_MATCHES",
+	})
+
+	return true, nil
+}
